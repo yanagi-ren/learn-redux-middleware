9216c492894131dd18081d9fc5c4ad4b604d1361
Liu8 <renxiang@Liu8ui-Macmini.local>
2020-09-17 23:01:16 +0900

    container, component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import CounterContainer from './containers/CounterContainer';
+
+import PostListContainer from './containers/PostListContainer';
 
 function App() {
   return (
-    <CounterContainer />
+    <PostListContainer />
   );
 }
 
