8ec3d8cef4e72e8f8c497825d007b364c979df4d
Liu8 <renxiang@Liu8ui-Macmini.local>
2020-09-17 22:17:15 +0900

    refactoring

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,5 +1,5 @@
 import * as postsAPI from '../api/posts';
-import { reducerUtils } from '../lib/asyncUtils';
+import { createPromiseThunk, handleAsyncActions, reducerUtils } from '../lib/asyncUtils';
 
 const GET_POSTS = 'GET_POSTS';
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
@@ -9,83 +9,28 @@ const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
-export const getPosts = () => async dispatch => {
-    // 요청이 시작됨
-    dispatch({ type: GET_POSTS });
-    // API를 호출
-    try {
-        const posts = await postsAPI.getPosts();
-        // 성공 했을 때
-        dispatch({
-            type: GET_POSTS_SUCCESS,
-            posts
-        });
-    } catch (e) {
-        // 실패했을 때
-        dispatch({
-            type: GET_POSTS_ERROR,
-            error: e
-        });
-    }
-};
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
 
-export const getPost = (id) => async dispatch => {
-    // 요청이 시작됨
-    dispatch({ type: GET_POST });
-    // API를 호출
-    try {
-        const post = await postsAPI.getPostById(id);
-        // 성공 했을 때
-        dispatch({
-            type: GET_POST_SUCCESS,
-            post
-        });
-    } catch (e) {
-        // 실패했을 때
-        dispatch({
-            type: GET_POST_ERROR,
-            error: e
-        });
-    }
-};
+export const getPost = createPromiseThunk(GET_POSTS, postsAPI.getPostById);
 
 const initialState = {
     posts: reducerUtils.initial(),
     post: reducerUtils.initial()
 }
 
+const getPostsReducer = handleAsyncActions(GET_POSTS, 'posts');
+const getPostReducer = handleAsyncActions(GET_POST, 'post');
+
 export default function posts(state = initialState, action) {
     switch (action.type) {
         case GET_POSTS:
-            return {
-                ...state,
-                posts: reducerUtils.loading()
-            };
         case GET_POSTS_SUCCESS:
-            return{
-                ...state,
-                posts: reducerUtils.success(action.posts)
-            };
         case GET_POSTS_ERROR:
-            return{
-                ...state,
-                posts: reducerUtils.error(action.error)
-            };
-            case GET_POST:
-                return {
-                    ...state,
-                    post: reducerUtils.loading()
-                };
-            case GET_POST_SUCCESS:
-                return{
-                    ...state,
-                    post: reducerUtils.success(action.post)
-                };
-            case GET_POST_ERROR:
-                return{
-                    ...state,
-                    post: reducerUtils.error(action.error)
-                };
+            return getPostsReducer(state, action);
+        case GET_POST:
+        case GET_POST_SUCCESS:        
+        case GET_POST_ERROR:
+            return getPostReducer(state, action);
         default:
             return state;
     }
