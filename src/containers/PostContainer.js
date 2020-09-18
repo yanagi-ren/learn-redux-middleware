3016d7d6ad99ca291e1fe35acdf134c081677048
Liu8 <renxiang@Liu8ui-Macmini.local>
2020-09-18 23:10:53 +0900

    Change structure of Postdata 's state

--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -1,20 +1,19 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Post from '../components/Post';
-import { getPost, clearPost } from '../modules/posts';
+import { reducerUtils } from '../lib/asyncUtils';
+import { getPost } from '../modules/posts';
 
 function PostContainer({ postId }) {
-    const { data, loading, error } = useSelector(state => state.posts.post);
+    const { data, loading, error } = useSelector(state => state.posts.post[postId] || reducerUtils.initial());
     const dispatch = useDispatch();
 
     useEffect(() => {
+        if (data) return;
         dispatch(getPost(postId));
-        return () => {
-            dispatch(clearPost());
-        };
-    }, [postId, dispatch]);
+    }, [postId, dispatch, data]);
 
-    if (loading) return <div>loading..</div>
+    if (loading && !data) return <div>loading..</div>
     if (error) return <div>error!</div>
     if (!data) return null;
 
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -21,17 +21,61 @@ const CLEAR_POST = 'CLEAR_POST';
 
 // 아주 쉽게 thunk 함수를 만들 수 있게 되었습니다.
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+export const getPost = id => async dispatch => {
+  dispatch({ type: GET_POST, meta: id });
+  try {
+    const payload = await postsAPI.getPostById(id);
+    dispatch({ type: GET_POST_SUCCESS, payload, meta: id })
+  } catch (e) {
+    dispatch({
+      type: GET_POSTS_ERROR,
+      payload: e,
+      error: true,
+      meta: id
+    })
+  }
+};
+
 export const clearPost = () => ({ type: CLEAR_POST });
 
 // initialState 쪽도 반복되는 코드를 initial() 함수를 사용해서 리팩토링 했습니다.
 const initialState = {
   posts: reducerUtils.initial(),
-  post: reducerUtils.initial()
+  post: {}
 };
 
 const getPostsReducer = handleAsyncActions(GET_POSTS, 'posts', true);
-const getPostReducer = handleAsyncActions(GET_POST, 'post');
+const getPostReducer = (state, action) => {
+  const id = action.meta;
+  switch (action.type) {
+    case GET_POST:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.loading(state.post[id] && state.post[id].data)
+        }
+      };
+    case GET_POST_SUCCESS:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.success(action.payload)
+        }
+      };
+    case GET_POST_ERROR:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.error(action.payload)
+        }
+      };
+    default:
+      return state;
+  }
+}
 
 export default function posts(state = initialState, action) {
   switch (action.type) {
